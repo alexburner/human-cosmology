c66c4afcbd5ff0b0cbd15eda1ecd66df67eb354a
Alex Burner <alex.burner@gmail.com>
2023-03-13 21:05:10 -0700

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
 import { FC } from 'react'
+import { layers } from './data/layers'
 
-export const App: FC = () => <div className="p-6">Hello World</div>
+export const App: FC = () => (
+  <div className="scroll-container">
+    <div className="scroll-content">
+      <div className="levels">
+        {layers.map((layer) => (
+          <div key={layer.name} className="level">
+            <div className="image" />
+            <span className="label">{layer.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+)
---
+++ src/data/layers.ts
@@ -0,0 +1,75 @@
+interface Layer {
+  name: string
+  src: string
+}
+
+export const layers: Layer[] = [
+  {
+    name: 'Big Bang',
+    src: '',
+  },
+  {
+    name: 'Cosmic Web',
+    src: '',
+  },
+  {
+    name: 'Galaxies',
+    src: '',
+  },
+  {
+    name: 'Stars',
+    src: '',
+  },
+  {
+    name: 'Planets',
+    src: '',
+  },
+  {
+    name: 'Geomes',
+    src: '',
+  },
+  {
+    name: 'Biomes',
+    src: '',
+  },
+  {
+    name: 'Organ-isms',
+    src: '',
+  },
+  {
+    name: 'Conscious Groups',
+    src: '',
+  },
+  {
+    name: 'Conscious Individuals',
+    src: '',
+  },
+  {
+    name: 'Organs',
+    src: '',
+  },
+  {
+    name: 'Cells',
+    src: '',
+  },
+  {
+    name: 'Biomolecules',
+    src: '',
+  },
+  {
+    name: 'Molecules',
+    src: '',
+  },
+  {
+    name: 'Atoms',
+    src: '',
+  },
+  {
+    name: 'Particles',
+    src: '',
+  },
+  {
+    name: 'Quantum Foam',
+    src: '',
+  },
+]
