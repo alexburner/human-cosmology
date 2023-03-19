66c6dee886b842c68e863af55216d91df3bd4036
Alex Burner <alex.burner@gmail.com>
2023-03-19 15:40:14 -0700

    Revert "Add intro text"
    This reverts commit 67f4e3c8cf6dff0ebde9b99d02299b1b24ab26de.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import { beginning, emergeOutOf, emergeWithin, Layer } from './data/layers'
 
 export const App: FC = () => (
   <div className="container">
-    <div className="row">
-      <div>emergent levels of cosmological complexity</div>
-    </div>
-    <div className="row emerges">
-      <div>emerging within</div>
-      <div>emerging out of</div>
-    </div>
     {new Array(8).fill(null).map((_, i) => {
       const index = 8 - i - 1
-      const isLast = i === 8 - 1
       const layerIn = emergeWithin[index]
       const layerOut = emergeOutOf[index]
       if (!layerIn || !layerOut) throw new Error('Unreachable')
       return (
-        <div key={i} className={'row' + (isLast ? ' last' : '')}>
+        <div key={i} className="row">
           <Cell layer={layerIn} />
           <Cell layer={layerOut} />
         </div>
